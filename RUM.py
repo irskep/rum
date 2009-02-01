193dbb18c42be164454a1e8b5646e44118e584c5
Stephen Johnson <srj15@case.edu>
2009-02-01 00:10:40 -0500

    Refactored RumSwigger

--- RUM.py
+++ RUM.py
@@ -28,50 +28,30 @@ class RumSwigger(object):
         self.string = ""
         
         cmd_dict = {
+            '>': self.inc_ptr,
+            '<': self.dec_ptr,
+            '+': self.inc_tape,
+            '-': self.dec_tape,
             '.': self.put_char,
             ',': self.get_char,
+            '[': self.check_bracket,
+            ']': self.check_bracket,
             '(': self.put_proc,
+            ')': self.end_proc,
             ':': self.call_proc,
-            '"': self.put_string
+            '"': self.put_string,
+            '#': self.skip_comment
         }
         while self.pgm_pos < self.pgm_len and self.pgm_pos >= 0:
             c = self.program[self.pgm_pos]
             if c in cmd_dict:
                 cmd_dict[c]()
-            elif c == '>':
-                self.ptr_pos += self.get_reps()
-                self.ptr_pos = self.check_tape(self.ptr_pos)
-            elif c == '<':
-                self.ptr_pos -= self.get_reps()
-                self.ptr_pos = self.check_tape(self.ptr_pos)
-            elif c == '+':
-                v = self.tape[self.ptr_pos] + self.get_reps()
-                if self.cell_size > 0:
-                    v = v % self.cell_size
-                self.tape[self.ptr_pos] = v
-            elif c == '-':
-                v = self.tape[self.ptr_pos] - self.get_reps()
-                if self.cell_size > 0:
-                    v = v % self.cell_size
-                self.tape[self.ptr_pos] = v
-            elif c == ')':
-                self.pgm_pos = self.return_positions.pop()
-            elif c == ']':
-                if self.tape[self.ptr_pos] != 0:
-                    self.go_to_matching_bracket()
-            elif c == '[':
-                if self.tape[self.ptr_pos] == 0:
-                    self.go_to_matching_bracket()
             elif c in string.digits:
                 if self.reps == 0:
                     self.reps = int(c)
                 else:
                     self.reps *= 10
                     self.reps += int(c)
-            elif c == "#":
-                while self.pgm_pos < self.pgm_len \
-                        and self.program[self.pgm_pos] != "\n":
-                    self.pgm_pos += 1
             self.pgm_pos += 1
         sys.stdout.write('\n')
     
@@ -82,6 +62,26 @@ class RumSwigger(object):
             return pos % self.tape_len
         self.tape.extend([0] * len(self.tape))
         return pos
+    
+    def inc_ptr(self):
+        self.ptr_pos += self.get_reps()
+        self.ptr_pos = self.check_tape(self.ptr_pos)
+    
+    def dec_ptr(self):
+        self.ptr_pos -= self.get_reps()
+        self.ptr_pos = self.check_tape(self.ptr_pos)
+    
+    def inc_tape(self):
+        v = self.tape[self.ptr_pos] + self.get_reps()
+        if self.cell_size > 0:
+            v = v % self.cell_size
+        self.tape[self.ptr_pos] = v
+    
+    def dec_tape(self):
+        v = self.tape[self.ptr_pos] - self.get_reps()
+        if self.cell_size > 0:
+            v = v % self.cell_size
+        self.tape[self.ptr_pos] = v
     
     def put_char(self):
         if self.tape[self.ptr_pos] in range(256):
@@ -105,10 +105,12 @@ class RumSwigger(object):
         else:
             self.tape[self.ptr_pos] = char_in
     
-    def go_to_matching_bracket(self):
+    def check_bracket(self):
         depth = 1
         start = self.pgm_pos
         if self.program[self.pgm_pos] == "]":
+            if self.tape[self.ptr_pos] == 0:
+                return
             while depth > 0 and self.pgm_pos >= 0:
                 self.pgm_pos -= 1
                 if self.program[self.pgm_pos] == "]":
@@ -118,6 +120,8 @@ class RumSwigger(object):
             if self.pgm_pos < 0:
                 print "Missing left bracket to match right at", start
         elif self.program[self.pgm_pos] == "[":
+            if self.tape[self.ptr_pos] != 0:
+                return
             while depth > 0 and self.pgm_pos < self.pgm_len:
                 self.pgm_pos += 1
                 if self.program[self.pgm_pos] == "]":
@@ -168,6 +172,14 @@ class RumSwigger(object):
             self.reps -= 1
         self.reps = 0
         self.pgm_pos = proc_pos
+    
+    def end_proc(self):
+        self.pgm_pos = self.return_positions.pop()
+    
+    def skip_comment(self):
+        while self.pgm_pos < self.pgm_len \
+                and self.program[self.pgm_pos] != "\n":
+            self.pgm_pos += 1
     
     def get_reps(self):
         r = max(1, self.reps)
